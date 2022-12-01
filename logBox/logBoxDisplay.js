7b6f63ae28c353b704d2d3a6dccad57c82c92050
Nina Emiko <nina.emiko.mcnair@gmail.com>
2022-11-30 23:57:39 -0500

    Revert "Updat action log feature, update UI"
    This reverts commit 68d1ac39d2b9e6cdd5ffe25480320a757f0ffbdf.

--- logBox/logBoxDisplay.js
+++ logBox/logBoxDisplay.js
@@ -1,12 +1,10 @@
 import { logBox } from "./logBox.js";
 
 const logBoxDisplayElement = document.getElementById('logBoxDisplayElement');
-//logBoxDisplayElement.scrolltop = logBoxDisplayElement.scrollHeight - logBoxDisplayElement.clientHeight;
 
 export function logBoxDisplay() {
-    let display = "";
 
-    logBox.forEach(element => display += ` ${element} <br>`)
-    
-    logBoxDisplayElement.innerHTML=display;
+    for (let i = 0; i < logBox.length; i++) {
+        logBoxDisplayElement.innerHTML=logBox[i];
+    }
 }
