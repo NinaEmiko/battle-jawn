fee122bb7c27905eb69039ddd4306f500909c0ef
Nina Emiko <nina.emiko.mcnair@gmail.com>
2022-12-02 09:00:41 -0500

    Fix player immortality bug

--- player/playerMove.js
+++ player/playerMove.js
@@ -2,24 +2,21 @@ import { playerAttack } from '../player/playerAttack.js';
 import { playerHeal } from '../player/playerHeal.js';
 import { playerRun } from '../player/playerRun.js';
 import { playerSteal } from '../player/playerSteal.js';
-import { enemyAttack } from '../enemy/enemyAttack.js';
-import { userInterface } from '../user/UI.js'
-import { logBoxDisplay } from '../logBox/logBoxDisplay.js';
 import { isAlive } from '../user/isAlive.js';
 
 export const playerMove = (input) => {
 
     if (input === 'attack') {
-        //isAlive();
+        isAlive();
         playerAttack();
     } else if (input === 'heal') {
-        //isAlive();
+        isAlive();
         playerHeal();
     } else if (input === 'run') {
-        //isAlive();
+        isAlive();
         playerRun();
     } else if (input === 'steal') {
-        //isAlive();
+        isAlive();
         playerSteal();
     }
 }
--- user/isAlive.js
+++ user/isAlive.js
@@ -5,7 +5,7 @@ export const isAlive = () => {
     if (user.health > 0) {
         return true;
     } else {
-        logBox.push('You have been slain!');
-        //window.location.reload();
+        confirm('You have been slain!');
+        window.location.reload();
     }
 }
