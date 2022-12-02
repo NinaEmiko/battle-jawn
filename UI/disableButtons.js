e3d42bc12bdeafa560a4ea8ee7ee138045369415
Nina Emiko <nina.emiko.mcnair@gmail.com>
2022-12-02 11:15:55 -0500

    Remove player ability to spam attack

--- UI/disableButtons.js
+++ UI/disableButtons.js
@@ -1,4 +1,35 @@
+let attack = document.getElementById("attack");
+let heal = document.getElementById("heal");
+let steal = document.getElementById("steal");
+let run = document.getElementById("run");
+
+function disableAttackButton() {
+    attack.disabled = true;
+    setTimeout(()=>{
+      attack.disabled = false}, 3000)
+  }
+
+function disableHealButton() {
+    heal.disabled = true;
+    setTimeout(()=>{
+        heal.disabled = false}, 3000)
+}
+
+function disableStealButton() {
+    steal.disabled = true;
+    setTimeout(()=>{
+      steal.disabled = false}, 3000)
+  }
+
+function disableRunButton() {
+    run.disabled = true;
+    setTimeout(()=>{
+        run.disabled = false}, 3000)
+}
 
 export function disableButtons() {
-
+    disableAttackButton();
+    disableHealButton();
+    disableStealButton();
+    disableRunButton();
 }
--- player/playerMove.js
+++ player/playerMove.js
@@ -3,20 +3,25 @@ import { playerHeal } from '../player/playerHeal.js';
 import { playerRun } from '../player/playerRun.js';
 import { playerSteal } from '../player/playerSteal.js';
 import { isAlive } from '../user/isAlive.js';
+import { disableButtons } from '../UI/disableButtons.js';
 
 export const playerMove = (input) => {
 
     if (input === 'attack') {
         isAlive();
         playerAttack();
+        disableButtons();
     } else if (input === 'heal') {
         isAlive();
         playerHeal();
+        disableButtons();
     } else if (input === 'run') {
         isAlive();
         playerRun();
+        disableButtons();
     } else if (input === 'steal') {
         isAlive();
         playerSteal();
+        disableButtons();
     }
 }
