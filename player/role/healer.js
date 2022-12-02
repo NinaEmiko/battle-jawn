806f65c8a93fd72adc3cc0ec830ce12e0e1df7ce
Nina Emiko <nina.emiko.mcnair@gmail.com>
2022-12-02 18:10:31 -0500

    Refactor classes

--- player/role/healer.js
+++ player/role/healer.js
@@ -3,11 +3,5 @@ import { Player } from "../player.js";
 export class Healer extends Player {
     constructor(name, role, health, maxHealth, strength, potions){
         super(name, role, health, maxHealth, strength, potions);
-        this.name = name;
-        this.role = role;
-        this.health = health;
-        this.maxHealth = maxHealth;
-        this.strength = strength;
-        this.potions = potions;
     }
 }
