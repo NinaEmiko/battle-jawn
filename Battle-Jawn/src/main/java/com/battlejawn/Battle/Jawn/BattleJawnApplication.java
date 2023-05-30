b207c354e8b446e98e75191cb81cead5f0ec45c6
Nina Emiko <nina.emiko.mcnair@gmail.com>
2023-05-30 12:44:51 -0400

    Began migration of Player, Enemy and Moves to backend with Java

---
+++ Battle-Jawn/src/main/java/com/battlejawn/Battle/Jawn/BattleJawnApplication.java
@@ -0,0 +1,13 @@
+package com.battlejawn.Battle.Jawn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleJawnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleJawnApplication.class, args);
+	}
+
+}
---
+++ Battle-Jawn/src/test/java/com/battlejawn/Battle/Jawn/BattleJawnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battlejawn.Battle.Jawn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleJawnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
