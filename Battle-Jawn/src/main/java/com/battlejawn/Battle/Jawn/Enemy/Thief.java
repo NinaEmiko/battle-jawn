c74bd4fa886a29104dc91226a07f6d2165a23e6b
Nina Emiko <nina.emiko.mcnair@gmail.com>
2023-06-02 16:23:59 -0400

    Updated Thief package/Made Thief subclass of Enemy

--- Battle-Jawn/src/main/java/com/battlejawn/Battle/Jawn/Enemy/Thief.java
+++ Battle-Jawn/src/main/java/com/battlejawn/Battle/Jawn/Enemy/Thief.java
@@ -1,6 +1,6 @@
-package main.java.com.battlejawn.Battle.Jawn.Enemy;
+package com.battlejawn.Battle.Jawn.Enemy;
 
-public class Thief {
+public class Thief extends Enemy {
 
     public Thief(String name, int health, int maxHealth, int potions, int strength) {
         super("Thief", 90, 90, 4, 17);
