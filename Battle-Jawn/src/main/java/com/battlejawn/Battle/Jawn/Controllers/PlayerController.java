2041d7bd13293a451ebd562781e91280eef63b16
Nina Emiko <nina.emiko.mcnair@gmail.com>
2023-06-02 16:19:33 -0400

    Updated PlayerActionController and PlayerController imports

--- Battle-Jawn/src/main/java/com/battlejawn/Battle/Jawn/Controllers/PlayerController.java
+++ Battle-Jawn/src/main/java/com/battlejawn/Battle/Jawn/Controllers/PlayerController.java
@@ -1,56 +1,58 @@
-package main.java.com.battlejawn.Battle.Jawn.Controllers;
+package com.battlejawn.Battle.Jawn.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlayerController {
-    private final PlayerService playerService;
+    // private final PlayerService playerService;
 
-    @Autowired
-    public PlayerController(PlayerService playerService) {
-        this.playerService = playerService;
-    }
+    // @Autowired
+    // public PlayerController(PlayerService playerService) {
+    //     this.playerService = playerService;
+    // }
 
-    @PostMapping("../PlayerAction/PlayerAction")
-    public void playerAction(@RequestBody InputDTO input) {
-        playerService.move(input);
-    }
+    // @PostMapping("../PlayerAction/PlayerAction")
+    // public void playerAction(@RequestBody InputDTO input) {
+    //     playerService.move(input);
+    // }
 
-    @PostMapping("../Player/Tank")
-    public ResponseEntity<String> createTank(@RequestBody TankDTO tankDTO) {
-        try {
-            playerService.createTank(tankDTO);
-            return ResponseEntitiy.ok("Tank created successfully");
-        } catch (Eception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create Tank");
-        }
-    }
+    // @PostMapping("../Player/Tank")
+    // public ResponseEntity<String> createTank(@RequestBody TankDTO tankDTO) {
+    //     try {
+    //         playerService.createTank(tankDTO);
+    //         return ResponseEntitiy.ok("Tank created successfully");
+    //     } catch (Eception e) {
+    //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create Tank");
+    //     }
+    // }
 
-    @PostMapping("../Player/Healer")
-    public ResponseEntity<String> createHealer(@RequestBody HealerDTO healerDTO) {
-        try {
-            playerService.createHealer(healerDTO);
-            return ResponseEntitiy.ok("Healer created successfully");
-        } catch (Eception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create Healer");
-        }
-    }
+    // @PostMapping("../Player/Healer")
+    // public ResponseEntity<String> createHealer(@RequestBody HealerDTO healerDTO) {
+    //     try {
+    //         playerService.createHealer(healerDTO);
+    //         return ResponseEntitiy.ok("Healer created successfully");
+    //     } catch (Eception e) {
+    //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create Healer");
+    //     }
+    // }
 
-    @PostMapping("../Player/Caster")
-    public ResponseEntity<String> createCaster(@RequestBody CasterDTO casterDTO) {
-        try {
-            playerService.createCaster(casterDTO);
-            return ResponseEntitiy.ok("Caster created successfully");
-        } catch (Eception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create Caster");
-        }
-    }
+    // @PostMapping("../Player/Caster")
+    // public ResponseEntity<String> createCaster(@RequestBody CasterDTO casterDTO) {
+    //     try {
+    //         playerService.createCaster(casterDTO);
+    //         return ResponseEntitiy.ok("Caster created successfully");
+    //     } catch (Eception e) {
+    //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create Caster");
+    //     }
+    // }
 
-    @PostMapping("../Player/DPS")
-    public ResponseEntity<String> createDPS(@RequestBody DPSDTO dpsDTO) {
-        try {
-            playerService.createDPS(dpsDTO);
-            return ResponseEntitiy.ok("DPS created successfully");
-        } catch (Eception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create DPS");
-        }
-    }
+    // @PostMapping("../Player/DPS")
+    // public ResponseEntity<String> createDPS(@RequestBody DPSDTO dpsDTO) {
+    //     try {
+    //         playerService.createDPS(dpsDTO);
+    //         return ResponseEntitiy.ok("DPS created successfully");
+    //     } catch (Eception e) {
+    //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create DPS");
+    //     }
+    // }
 }
