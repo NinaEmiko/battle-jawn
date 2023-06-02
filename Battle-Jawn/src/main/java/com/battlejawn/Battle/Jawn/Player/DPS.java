4900b9e11427b8b75ebf49f218d1bb9db078aab7
Nina Emiko <nina.emiko.mcnair@gmail.com>
2023-06-02 18:05:09 -0400

    Resolved errors inhibiting tomcat server

--- Battle-Jawn/src/main/java/com/battlejawn/Battle/Jawn/Player/DPS.java
+++ Battle-Jawn/src/main/java/com/battlejawn/Battle/Jawn/Player/DPS.java
@@ -1,42 +1,42 @@
 package com.battlejawn.Battle.Jawn.Player;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.EnumType;
+// import jakarta.persistence.Enumerated;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
 
 import com.battlejawn.Battle.Jawn.StatusAilments.StatusAilments;
 
-@Entity
-@Table(name = "dps")
+// @Entity
+// @Table(name = "dps")
 public class DPS extends Player {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
-    @Column(name = "health")
-    private int health;
-    @Column(name = "max_health")
-    private int maxHealth;
-    @Column(name = "strength")
-    private int strength;
-    @Column(name = "potions")
-    private int potions;
-    @Column(name = "maxPotions")
-    private int maxPotions;
-    @Enumerated(EnumType.STRING)
-    @Column(name = "status_ailments")
-    private StatusAilments statusAilments;
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @Column(name = "id")
+    // private Long id;
+    // @Column(name = "health")
+    // private int health;
+    // @Column(name = "max_health")
+    // private int maxHealth;
+    // @Column(name = "strength")
+    // private int strength;
+    // @Column(name = "potions")
+    // private int potions;
+    // @Column(name = "maxPotions")
+    // private int maxPotions;
+    // @Enumerated(EnumType.STRING)
+    // @Column(name = "status_ailments")
+    // private StatusAilments statusAilments;
 
     public DPS(Long id, int health, int maxHealth, int strength, int potions,
             int maxPotions, StatusAilments statusAilments) {
         super(90, 90, 20, 2, 3, statusAilments);
-        this.id = id;
+        // this.id = id;
     }
 
 }
