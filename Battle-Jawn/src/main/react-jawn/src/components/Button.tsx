5045a252f4df4cc43ac9eb97da2b91c3b998bc03
Nina Emiko <nina.emiko.mcnair@gmail.com>
2023-06-16 11:11:34 -0400

    Added styling for button component

--- Battle-Jawn/src/main/react-jawn/src/components/Button.tsx
+++ Battle-Jawn/src/main/react-jawn/src/components/Button.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from "react";
+import "../styling/Button.css";
 
 const Button = () => {
   return (
     <>
-    <button className="btn" id="button1">Button 1</button>
-    <button className="btn" id="button2">Button 2</button>
-    <button className="btn" id="button3">Button 3</button>
-    <button className="btn" id="button4">Button 4</button>
+      <div className="btn-grid" id="option-buttons">
+        <button className="btn" id="button1">
+          Button 1
+        </button>
+        <button className="btn" id="button2">
+          Button 2
+        </button>
+        <button className="btn" id="button3">
+          Button 3
+        </button>
+        <button className="btn" id="button4">
+          Button 4
+        </button>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
