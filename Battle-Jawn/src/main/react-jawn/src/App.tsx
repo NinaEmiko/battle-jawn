3c6412194ff262f61bddfca5fa404e509e94a7b5
Nina Emiko <nina.emiko.mcnair@gmail.com>
2023-06-16 10:23:35 -0400

    Added react component for buttons

--- Battle-Jawn/src/main/react-jawn/src/App.tsx
+++ Battle-Jawn/src/main/react-jawn/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css'
+import Button from './components/Button';
 
 function App() {
-
+  return (
+    <>
+      <Button></Button>
+    </>
+  );
 }
 
 export default App
---
+++ Battle-Jawn/src/main/react-jawn/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <>
+    <button>Button 1</button>
+    <button>Button 2</button>
+    <button>Button 3</button>
+    <button>Button 4</button>
+    </>
+  )
+}
+
+export default Button
