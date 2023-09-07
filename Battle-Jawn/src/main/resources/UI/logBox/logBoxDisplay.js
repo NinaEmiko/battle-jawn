a99f831c1eefba3c409893debae3cbcfe6247072
Nina McNair <nina@Ninas-MacBook-Pro.local>
2023-09-07 07:54:20 -0400

    Removed duplicate files

--- Battle-Jawn/src/main/resources/UI/logBox/logBoxDisplay.js
+++
@@ -1,9 +0,0 @@
-import { logBox } from "../logBox/logBox.js";
-
-const logBoxDisplayElement = document.getElementById('logBoxDisplayElement');
-
-export function logBoxDisplay() {
-    let display = "";
-    logBox.forEach(element => display += ` ${element} <br>`)
-    logBoxDisplayElement.innerHTML=display;
-}
