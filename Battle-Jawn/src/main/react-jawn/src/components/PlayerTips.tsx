3dd9e37d0c07e983bc9ce65b44bf89ff43bd2891
Nina McNair <nina@Ninas-MacBook-Pro.local>
2023-11-22 22:26:40 -0500

    Made random player top get request asynchronis

--- Battle-Jawn/src/main/react-jawn/src/components/PlayerTips.tsx
+++ Battle-Jawn/src/main/react-jawn/src/components/PlayerTips.tsx
@@ -6,15 +6,18 @@ const PlayerTips = () => {
   const [randomTip, setRandomTip] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:8080/api/player-tip/random')
-      .then((response) => {
-        const randomTipBody = response.data;
-        setRandomTip(randomTipBody);
-      })
-      .catch((error) => {
-        console.error('Error fetching random player tip:', error);
-      });
-  }, []);
+          const fetchPlayerTip = async () => {
+              try {
+                  const response = await
+                  axios.get('http://localhost:8080/api/player-tip/random')
+                  const randomTipBody = response.data;
+                          setRandomTip(randomTipBody);
+                  } catch (error) {
+                  console.error('Error fetching Player Tip data: ', error)
+                  }
+              }
+          fetchPlayerTip();
+      }, [])
 
   return (
     <div>
