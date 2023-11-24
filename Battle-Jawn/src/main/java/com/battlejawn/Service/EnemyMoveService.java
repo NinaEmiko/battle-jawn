aac42119ecec4d027258e5389d446709b949d2f2
Nina McNair <nina@Ninas-MacBook-Pro.local>
2023-11-24 10:05:00 -0500

    Updateed enemy move logic

--- Battle-Jawn/src/main/java/com/battlejawn/Service/EnemyMoveService.java
+++ Battle-Jawn/src/main/java/com/battlejawn/Service/EnemyMoveService.java
@@ -1,36 +1,71 @@
 package com.battlejawn.Service;
 
+import com.battlejawn.Config.HeroMoveDTO;
 import com.battlejawn.EnemyMove.Strike;
+import com.battlejawn.Entities.Battle.BattleSession;
+import com.battlejawn.Entities.Enemy.Enemy;
 import com.battlejawn.Entities.Hero.Hero;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EnemyMoveService {
     private final HeroService heroService;
     private final BattleHistoryMessageService battleHistoryMessageService;
+    private final EnemyService enemyService;
+    private final BattleSessionService battleSessionService;
+    private HeroMoveDTO heroMoveDTO;
 
-    public EnemyMoveService(HeroService heroService, BattleHistoryMessageService battleHistoryMessageService) {
+    public EnemyMoveService(BattleSessionService battleSessionService, EnemyService enemyService, HeroService heroService, BattleHistoryMessageService battleHistoryMessageService) {
+        this.battleSessionService = battleSessionService;
+        this.enemyService = enemyService;
         this.heroService = heroService;
         this.battleHistoryMessageService = battleHistoryMessageService;
     }
 
-    public void useEnemyMove(Long heroId, Long battleSessionId){
+    public HeroMoveDTO useEnemyMove(Long heroId, Long battleSessionId){
+        Enemy enemy = enemyService.getEnemyById(battleSessionService.getBattleSessionById(battleSessionId).getEnemyId());
         Strike strike = new Strike();
         int damage = strike.attack();
         Hero hero = heroService.getHeroById(heroId);
         int updatedHeroHealth;
+        String newMessage;
         if (damage > hero.getHealth()) {
             updatedHeroHealth = 0;
+            newMessage = getDamageMessage("Strike", damage);
+            heroService.updateHealthById(updatedHeroHealth, hero.getId());
+            String heroDefeatedMessage = "You have been defeated by the enemy!";
+            battleHistoryMessageService.createNewMessage(battleSessionId, newMessage);
+            battleHistoryMessageService.createNewMessage(battleSessionId, heroDefeatedMessage);
+            List<String> battleHistory = battleHistoryMessageService.getBattleHistoryMessagesByBattleSessionId(battleSessionId);
+            return heroMoveDTO = getHeroMoveReturnObject(enemy.getHealth(), hero.getHealth(), hero.getPotions(), battleHistory, true);
         } else {
             updatedHeroHealth = hero.getHealth() - damage;
+            newMessage = getDamageMessage("Strike", damage);
+            heroService.updateHealthById(updatedHeroHealth, hero.getId());
+            battleHistoryMessageService.createNewMessage(battleSessionId, newMessage);
+            List<String> battleHistory = battleHistoryMessageService.getBattleHistoryMessagesByBattleSessionId(battleSessionId);
+            return heroMoveDTO = getHeroMoveReturnObject(enemy.getHealth(), hero.getHealth(), hero.getPotions(), battleHistory, true);
         }
-        heroService.updateHealthById(updatedHeroHealth, hero.getId());
-        String newMessage;
-        if (damage > 0) {
-            newMessage = "Enemy strike" + " did " + damage + " damage.";
-        } else {
-            newMessage = "Enemy strike missed!";
-        }
-        battleHistoryMessageService.createNewMessage(battleSessionId, newMessage);
     }
+
+    public String getDamageMessage(String move, int damage) {
+        if (damage > 0) {
+            return move + " did " + damage + " damage.";
+        } else {
+            return move + " missed!";
+        }
+    }
+
+    public HeroMoveDTO getHeroMoveReturnObject(int enemyHealth, int heroHealth, int potionCount, List<String> battleHistory, boolean gameOver) {
+        HeroMoveDTO heroMoveDTO = new HeroMoveDTO();
+        heroMoveDTO.setEnemyHealth(enemyHealth);
+        heroMoveDTO.setHeroHealth(heroHealth);
+        heroMoveDTO.setPotionCount(potionCount);
+        heroMoveDTO.setBattleHistory(battleHistory);
+        heroMoveDTO.setGameOver(gameOver);
+        return heroMoveDTO;
+    }
+
 }
