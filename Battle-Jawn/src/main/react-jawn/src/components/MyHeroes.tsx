6366bededc873a1d74ae08fd386c0568173007a3
Nina McNair <nina@Ninas-MacBook-Pro.local>
2023-12-10 21:23:45 -0500

    Updated Hero List component

--- Battle-Jawn/src/main/react-jawn/src/components/MyHeroes.tsx
+++ Battle-Jawn/src/main/react-jawn/src/components/MyHeroes.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import classNames from 'classnames';
 
 function MyHeroes( {props}:{props:any} ) {
     const [chooseHero, setChooseHero] = useState(false);
@@ -38,10 +39,41 @@ function MyHeroes( {props}:{props:any} ) {
     <div className="">
     {heroList.map((hero) => (
   <div className="container-jawn-hero-card" key={hero.id}>
-    <p>Hero: {hero.role}</p>
-    <p>Hero Win Count: {hero.winCount}</p>
-    <p>Hero Loss Count: {hero.lossCount}</p>
-    <p>Hero Run Count: {hero.runCount}</p>
+    <p className="title-jawn">Name Goes Here</p>
+    <table>
+      <tbody>
+        <tr>
+          <td className="row-jawn">Class:</td>
+          <td className="data-jawn">{hero.role}</td>
+        </tr>
+        <tr>
+          <td className="row-jawn">Health:</td>
+          <td className="data-jawn">{hero.health} / {hero.maxHealth}</td>
+        </tr>
+        <tr>
+          <td className="row-jawn">Potions:</td>
+          <td className="data-jawn">{hero.potions}</td>
+        </tr>
+        <tr>
+          <td className="row-jawn">Wins:</td>
+          <td className="data-jawn">{hero.winCount}</td>
+        </tr>
+        <tr>
+          <td className="row-jawn">Losses:</td>
+          <td className="data-jawn">{hero.lossCount}</td>
+        </tr>
+        <tr>
+          <td className="row-jawn">Run Count:</td>
+          <td className="data-jawn">{hero.runCount}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div className="row justify-content-center">
+          <button className={classNames('nav-link', 'btn', 'custom-button')} id="rest-btn">Rest</button>
+          <button className={classNames('nav-link', 'btn', 'custom-button')} id="delete-btn">Delete</button>
+        </div>
+      
+
     </div>
 ))}
 </div>
