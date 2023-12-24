9461cfde307f8679083e5ae4707971edbff01d38
Nina McNair <nina@Ninas-MacBook-Pro.local>
2023-12-23 21:40:43 -0500

    Update account settings styles

--- Battle-Jawn/src/main/react-jawn/src/components/AccountSettings.tsx
+++ Battle-Jawn/src/main/react-jawn/src/components/AccountSettings.tsx
@@ -1,15 +1,17 @@
 import { request, setAuthHeader } from "../helpers/axios_helper";
 import { useState } from 'react';
+import classNames from 'classnames';
 
 const AccountSettings = ({props}:{props:any}) => {
-  const [accountIsDeleted, setAccountIsDeleted] = useState(false);
   const [newPassword, setNewPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [showDeleteAccount, setShowDeleteAccount] = useState(false);
+  const [showUpdatePassword, setShowUpdatePassword] = useState(false);
+  const [active, setActive] = useState('')
 
-  const onDelete = async () => {
+  const onSubmitDelete = async () => {
     try {
       const response = await request('DELETE', `http://localhost:8080/delete/${props.id}`, {});
-        setAccountIsDeleted(true);
       setAuthHeader(null);
     }
     catch (error) {
@@ -28,17 +30,60 @@ const AccountSettings = ({props}:{props:any}) => {
     }
   }
 
+  const toggleShowSettings = (setting: string) => {
+    if (setting == 'update password') {
+      setShowUpdatePassword(!showUpdatePassword);
+      setActive(setting)
+      setShowDeleteAccount(false);
+    } else if (setting == 'delete account') {
+      setShowDeleteAccount(!showDeleteAccount);
+      setActive(setting)
+      setShowUpdatePassword(false);
+    }
+  }
+
   return (
-    <div className="container-jawn-login-form">
+    <div className="container-jawn-login-form log-reg">
+      <div className="row justify-content-center">
         <h1 className="title-jawn">Account Settings</h1>
-            <label className="account-settings-jawn">New Password:</label>
-            <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)}></input>
-            <button onClick={handlePasswordChange} className="btn">Update Password</button>
-              <p>{message}</p>
-            <button onClick={() => onDelete()} className="btn">Delete Account</button>
-            {accountIsDeleted && (
-              <p>Account successfully deleted</p>
+        <ul className="nav-justified mb-3 log-reg-2" id="ex1" role="tablist">
+          <li className="nav-item" role="presentation">
+            <button
+            className={classNames('nav-link', 'tab-update-password', 'btn', 'custom-button', active === 'update password' ? 'active' : '')}
+            id="tab-login"
+            onClick={(e) => toggleShowSettings('update password')}
+            >
+              Update Password
+            </button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button
+            className={classNames('nav-link', 'tab-delete-account', 'btn', 'custom-button', active === 'delete account' ? 'active' : '')}
+            id="tab-register"
+            onClick={(e) => toggleShowSettings('delete account')}
+            >
+              Delete Account
+            </button>
+          </li>
+
+        </ul>
+            {showUpdatePassword && (
+              <div>
+                  <div className="userPrompt">
+                      {"New Password: "}
+                  </div>
+                  <input className="hero-name-input" type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)}></input>
+                  <button onClick={handlePasswordChange} className="btn">Submit</button>
+                  <p>{message}</p>
+              </div>
             )}
+
+            {showDeleteAccount && (
+              <div>
+                <button onClick={() => onSubmitDelete()} className="btn">Submit</button>
+              </div>
+            )}
+      </div>
     </div>
   );
 };
