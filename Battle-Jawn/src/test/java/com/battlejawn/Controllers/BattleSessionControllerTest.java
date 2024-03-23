74d09b2987cacbbf0338972f0943973f1b74fb4f
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-03-23 19:31:39 -0400

    Added more junit

--- Battle-Jawn/src/test/java/com/battlejawn/Controllers/BattleSessionControllerTest.java
+++ Battle-Jawn/src/test/java/com/battlejawn/Controllers/BattleSessionControllerTest.java
@@ -1,7 +1,49 @@
 package com.battlejawn.Controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.battlejawn.Entities.Battle.BattleSession;
+import com.battlejawn.Service.BattleSessionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class BattleSessionControllerTest {
+    @Mock
+    BattleSessionService battleSessionService;
+    @Mock
+    BattleSession battleSession;
+    @InjectMocks
+    BattleSessionController battleSessionController;
+    @BeforeEach
+    void setup() {
+
+    }
+    @Test
+    void getBattleSessionByIdTest() {
+        when(battleSessionService.getBattleSessionById(anyLong())).thenReturn(battleSession);
+        battleSessionController.getBattleSessionById(anyLong());
+        verify(battleSessionService, times(1)).getBattleSessionById(anyLong());
+    }
+    @Test
+    void getBattleSessionByIdNullTest() {
+        when(battleSessionService.getBattleSessionById(anyLong())).thenReturn(null);
+        battleSessionController.getBattleSessionById(anyLong());
+        verify(battleSessionService, times(1)).getBattleSessionById(anyLong());
+    }
+    @Test
+    void createNewBattleSessionTest() {
+
+    }
+    @Test
+    void createNewBattleSessionNullTest() {
+
+    }
 
 }
