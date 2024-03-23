c4ec793beef934e85888db26092f512725acf8a0
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-03-23 19:13:32 -0400

    Added junit for hero controller

--- Battle-Jawn/src/test/java/com/battlejawn/Controllers/HeroMoveControllerTest.java
+++ Battle-Jawn/src/test/java/com/battlejawn/Controllers/HeroMoveControllerTest.java
@@ -1,7 +1,37 @@
 package com.battlejawn.Controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.battlejawn.Config.JsonParser;
+import com.battlejawn.DTO.HeroMoveDTO;
+import com.battlejawn.Service.HeroMoveService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class HeroMoveControllerTest {
+    @Mock
+    HeroMoveService heroMoveService;
+    @Mock
+    HeroMoveDTO heroMoveDTO;
+    @Mock
+    JsonParser jsonParser;
+    @InjectMocks
+    HeroMoveController heroMoveController;
+    @BeforeEach
+    void setup() {
+
+    }
+    @Test
+    void heroMoveTest() {
+
+    }
 
 }
