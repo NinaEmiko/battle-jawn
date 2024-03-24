6866878a096f41ca6591a352b693bb9cfb023e0c
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-03-24 14:16:44 -0400

    Added some more tests

--- Battle-Jawn/src/test/java/com/battlejawn/Service/HeroMoveServiceTest.java
+++ Battle-Jawn/src/test/java/com/battlejawn/Service/HeroMoveServiceTest.java
@@ -1,6 +1,7 @@
 package com.battlejawn.Service;
 
 import com.battlejawn.DTO.HeroMoveDTO;
+import com.battlejawn.Entities.Battle.BattleHistoryMessage;
 import com.battlejawn.Entities.Battle.BattleSession;
 import com.battlejawn.Entities.Enemy.Enemy;
 import com.battlejawn.Entities.Hero.Hero;
@@ -10,6 +11,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -41,6 +45,25 @@ class HeroMoveServiceTest {
     }
     @Test
     void heroMoveTest() {
+
+    }
+    @Test
+    void getHeroMoveReturnObjectTest() {
+        int expectedEnemyHealth = 1;
+        int expectedHeroHealth = 2;
+        int expectedPotionCount = 3;
+        List<String> battleHistory = new ArrayList<>();
+        String expectedBattleHistoryMessage = "Hi";
+        battleHistory.add(expectedBattleHistoryMessage);
+        boolean expectedGameOver = false;
+
+        HeroMoveDTO actual = heroMoveService.getHeroMoveReturnObject(1, 2, 3, battleHistory, false);
+
+        assertEquals(expectedEnemyHealth, actual.getEnemyHealth());
+        assertEquals(expectedHeroHealth, actual.getHeroHealth());
+        assertEquals(expectedPotionCount, actual.getPotionCount());
+        assertEquals(expectedBattleHistoryMessage, actual.getBattleHistory().get(0));
+        assertEquals(expectedGameOver, actual.isGameOver());
 
     }
 }
