a3ea19952bdfab56a6c5f7961c7b56d30f401daf
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-03-31 09:30:58 -0400

    Added unit tests for purge jobs

--- Battle-Jawn/src/test/java/com/battlejawn/Purge/BattleHistoryMessagePurgeTest.java
+++ Battle-Jawn/src/test/java/com/battlejawn/Purge/BattleHistoryMessagePurgeTest.java
@@ -1,7 +1,53 @@
 package com.battlejawn.Purge;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.battlejawn.Entities.Battle.BattleHistoryMessage;
+import com.battlejawn.Repository.BattleHistoryMessageRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class BattleHistoryMessagePurgeTest {
+    @Mock
+    BattleHistoryMessageRepository battleHistoryMessageRepository;
+    @Mock
+    List<BattleHistoryMessage> battleHistoryMessages;
+    @InjectMocks
+    BattleHistoryMessagePurge battleHistoryMessagePurge;
+    @BeforeEach
+    void setup(){
+        battleHistoryMessages = new ArrayList<>();
+        BattleHistoryMessage battleHistoryMessage = new BattleHistoryMessage();
+        battleHistoryMessage.setBattleSessionId(1L);
+        battleHistoryMessage.setCreatedAt(LocalDateTime.now().minusDays(6));
+        battleHistoryMessage.setMessage("This");
+        battleHistoryMessage.setBattleSessionId(2L);
+        battleHistoryMessages.add(battleHistoryMessage);
+    }
+    @Test
+    void purgeBattleHistoryMessagesTest() {
+        doNothing().when(battleHistoryMessageRepository).deleteById(any());
+        when(battleHistoryMessageRepository.findAll()).thenReturn(battleHistoryMessages);
+        battleHistoryMessagePurge.purgeBattleHistoryMessage();
+        verify(battleHistoryMessageRepository, times(1)).deleteById(any());
+    }
+
+    @Test
+    void purgeBattleHistoryMessagesExceptionTest() {
+        when(battleHistoryMessageRepository.findAll()).thenReturn(battleHistoryMessages);
+        doThrow(new RuntimeException()).when(battleHistoryMessageRepository).deleteById(anyLong());
+        assertThrows(RuntimeException.class, () -> battleHistoryMessagePurge.purgeBattleHistoryMessage());
+    }
 
 }
--- Battle-Jawn/src/test/java/com/battlejawn/Purge/EnemyPurgeTest.java
+++ Battle-Jawn/src/test/java/com/battlejawn/Purge/EnemyPurgeTest.java
@@ -1,7 +1,54 @@
 package com.battlejawn.Purge;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.battlejawn.Entities.Enemy.Enemy;
+import com.battlejawn.Entities.Enemy.Wolf;
+import com.battlejawn.Repository.EnemyRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class EnemyPurgeTest {
+    @Mock
+    EnemyRepository enemyRepository;
+    @Mock
+    List<Enemy> enemies;
+    @Mock
+    Enemy enemy;
+    @InjectMocks
+    EnemyPurge enemyPurge;
+    @BeforeEach
+    void setup() {
+        enemies = new ArrayList<>();
+        enemy = new Wolf();
+        enemy.setId(1L);
+        enemy.setCreatedAt(LocalDateTime.now().minusDays(6));
+        enemies.add(enemy);
+    }
+    @Test
+    void purgeEnemyTest() {
+        when(enemyRepository.findAll()).thenReturn(enemies);
+        doNothing().when(enemyRepository).deleteById(anyLong());
+        enemyPurge.purgeEnemy();
+        verify(enemyRepository, times(1)).deleteById(anyLong());
+    }
+    @Test
+    void purgeEnemyExceptionTest() {
+        when(enemyRepository.findAll()).thenReturn(enemies);
+        doThrow(new RuntimeException()).when(enemyRepository).deleteById(anyLong());
+        assertThrows(RuntimeException.class, () -> enemyPurge.purgeEnemy());
+    }
 
 }
