1167dfe9138c8d4f50585e9ff57c42f139c48823
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-04-15 20:58:39 -0400

    Removed legacy code

--- legacy/player/role/healer.js
+++
@@ -1,7 +0,0 @@
-import { Player } from "../player.js";
-
-export class Healer extends Player {
-    constructor(role, health, maxHealth, strength, potions, maxPotions, statusAilments){
-        super(role, health, maxHealth, strength, potions, maxPotions, statusAilments);
-    }
-}
--- legacy/player/statusAilments.js
+++
@@ -1,6 +0,0 @@
-export const statusAilments = {
-    slow: false,
-    bubble: false,
-    vulnerable: false,
-    paralyze: false
-};
