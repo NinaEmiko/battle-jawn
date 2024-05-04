1126e8008736f9c11b6374d4015732109663f293
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-05-03 22:12:42 -0400

    Added hero card carousel

--- frontend/src/components/MyHeroes.tsx
+++ frontend/src/components/MyHeroes.tsx
@@ -19,6 +19,7 @@ function MyHeroes( {props}:{props:any} ) {
   const [popUpType, setPopUpType] = useState("");
   const [popUpContent, setPopUpContent] = useState("");
   const [showPopUp, setShowPopUp] = useState(false);
+  const [currentHeroIndex, setCurrentHeroIndex] = useState(0);
 
   const navigate = useNavigate();
 
@@ -72,6 +73,7 @@ function MyHeroes( {props}:{props:any} ) {
     setPopUpContent("delete hero");
     setShowPopUp(true);
     setDeleteHeroId(id);
+    setCurrentHeroIndex(0);
   }
   
   const handleSubComponentButtonClick = (component: string) => {
@@ -81,6 +83,22 @@ function MyHeroes( {props}:{props:any} ) {
       setInventoryActive(false);
     }
   }
+
+  const previousHero = () => {
+    if(currentHeroIndex == 0) {
+      setCurrentHeroIndex(heroList.length - 1)
+    } else {
+      setCurrentHeroIndex(currentHeroIndex - 1);
+    }
+  };
+
+  const nextHero = () => {
+    if(currentHeroIndex == heroList.length - 1) {
+      setCurrentHeroIndex(0)
+    } else {
+      setCurrentHeroIndex(currentHeroIndex + 1);
+    }
+  };
     
   useEffect(() => {
     fetchHeroesCall();
@@ -115,62 +133,73 @@ function MyHeroes( {props}:{props:any} ) {
         {!battleActive && !inventoryActive && !storeActive && !showPopUp &&
 
           <div className="container-jawn-hero">
+            <div className="carousel-jawn">
+            {heroList.length > 1 &&
+              <button className={classNames('nav-link', 'btn', 'custom-button')} id="prev-btn" onClick={() => previousHero()}>Prev</button>
+            }   
             <h1 className="title-jawn-hero">{props.userName} Heroes</h1>
+            {heroList.length > 1 &&
+              <button className={classNames('nav-link', 'btn', 'custom-button')} id="next-btn" onClick={() => nextHero()}>Next</button>
+            }
+            </div>
             {heroList.length < 5 &&
             <div className="btn-cntr">
             <button className="btn" id="new-hero-btn" onClick={() => handleNavigation('/create-hero')}>Create New Hero</button>
             </div>
             }
+
+            {heroList.length > 0 &&
+
             <div className="">
-            {heroList.map((hero) => (
-          <div className="container-jawn-hero-card" key={hero.id}>
+          <div className="container-jawn-hero-card">
             <div className="hero-name-level">
-              <div className="hero-name"> {hero.name} </div>
-              <div className="hero-level"> Lvl {hero.level} {hero.role} </div>
+              <div className="hero-name"> {heroList[currentHeroIndex].name} </div>
+              <div className="hero-level"> Lvl {heroList[currentHeroIndex].level} {heroList[currentHeroIndex].role} </div>
             </div>
             
             <table className="my-heroes-table">
               <tbody>
                 <tr>
                   <td className="row-jawn">Health:</td>
-                  <td className="data-jawn" id="health-jawn">{hero.health} / {hero.maxHealth}</td>
+                  <td className="data-jawn" id="health-jawn">{heroList[currentHeroIndex].health} / {heroList[currentHeroIndex].maxHealth}</td>
                 </tr>
                 <tr>
                   <td className="row-jawn">Coins:</td>
-                  <td className="data-jawn" id="health-jawn">{hero.coins}</td>
+                  <td className="data-jawn" id="health-jawn">{heroList[currentHeroIndex].coins}</td>
                 </tr>
                 <tr>
                   <td className="row-jawn">Wins:</td>
-                  <td className="data-jawn">{hero.winCount}</td>
+                  <td className="data-jawn">{heroList[currentHeroIndex].winCount}</td>
                 </tr>
                 <tr>
                   <td className="row-jawn">Losses:</td>
-                  <td className="data-jawn">{hero.lossCount}</td>
+                  <td className="data-jawn">{heroList[currentHeroIndex].lossCount}</td>
                 </tr>
                 <tr>
                   <td className="row-jawn">Run Count:</td>
-                  <td className="data-jawn">{hero.runCount}</td>
+                  <td className="data-jawn">{heroList[currentHeroIndex].runCount}</td>
                 </tr>
               </tbody>
             </table>
             <div className="experience-bar-container">
-              <progress className='experience-bar' value={hero.experience} max={determineMaxExperience(hero.level)}></progress>
-              <span className="experience-fraction">{determineNumerator(hero.level, hero.experience)}/{determineMaxExperience(hero.level)}</span>
+              <progress className='experience-bar' value={heroList[currentHeroIndex].experience} max={determineMaxExperience(heroList[currentHeroIndex].level)}></progress>
+              <span className="experience-fraction">{determineNumerator(heroList[currentHeroIndex].level, heroList[currentHeroIndex].experience)}/{determineMaxExperience(heroList[currentHeroIndex].level)}</span>
             </div>
             <div className="row justify-content-center">
-                  <button onClick={() => handleStore(hero.id)} className={classNames('nav-link', 'btn', 'custom-button')} id="store-btn">Store</button>
-                  <button onClick={() => handleInventory(hero.id)} className={classNames('nav-link', 'btn', 'custom-button')} id="inventory-btn">Inventory</button>
-                  <button onClick={() => handleFight(hero.id, hero.health)} className={classNames('nav-link', 'btn', 'custom-button')} id="fight-btn">Fight</button>
+                  <button onClick={() => handleStore(heroList[currentHeroIndex].id)} className={classNames('nav-link', 'btn', 'custom-button')} id="store-btn">Store</button>
+                  <button onClick={() => handleInventory(heroList[currentHeroIndex].id)} className={classNames('nav-link', 'btn', 'custom-button')} id="inventory-btn">Inventory</button>
+                  <button onClick={() => handleFight(heroList[currentHeroIndex].id, heroList[currentHeroIndex].health)} className={classNames('nav-link', 'btn', 'custom-button')} id="fight-btn">Fight</button>
                   {import.meta.env.VITE_REACT_APP_URL == "http://localhost:8080" &&
-                  <button onClick={() => handleRest(hero.id)} className={classNames('nav-link', 'btn', 'custom-button')} id="rest-btn">Rest</button>
+                  <button onClick={() => handleRest(heroList[currentHeroIndex].id)} className={classNames('nav-link', 'btn', 'custom-button')} id="rest-btn">Rest</button>
                    }
-                  <button onClick={() => handleDeleteConfirmation(hero.id)} className={classNames('nav-link', 'btn', 'custom-button')} id="delete-btn">Delete</button>
+                  <button onClick={() => handleDeleteConfirmation(heroList[currentHeroIndex].id)} className={classNames('nav-link', 'btn', 'custom-button')} id="delete-btn">Delete</button>
             </div>
-              
+            
 
             </div>
-        ))}
+            
         </div>
+}
             </div>
         }
       </div>
