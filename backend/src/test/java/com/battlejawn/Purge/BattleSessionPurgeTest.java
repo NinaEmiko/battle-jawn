4292e7d1186f32da9474e9ef1b449d1f11539aad
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-05-03 23:52:09 -0400

    Updated junit

--- backend/src/test/java/com/battlejawn/Purge/BattleSessionPurgeTest.java
+++ backend/src/test/java/com/battlejawn/Purge/BattleSessionPurgeTest.java
@@ -1,7 +1,10 @@
 package com.battlejawn.Purge;
 
 import com.battlejawn.Entities.Battle.BattleSession;
+import com.battlejawn.Entities.Hero.Hero;
+import com.battlejawn.Entities.Hero.Tank;
 import com.battlejawn.Repository.BattleSessionRepository;
+import com.battlejawn.Service.HeroService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,28 +25,37 @@ public class BattleSessionPurgeTest {
     @Mock
     List<BattleSession> battleSessions;
     @Mock
+    HeroService heroService;
+    @Mock
+    Hero tank;
+    @Mock
     BattleSession battleSession;
     @InjectMocks
     BattleSessionPurge battleSessionPurge;
     @BeforeEach
     void setup() {
+        tank = new Tank("Name");
         battleSession = new BattleSession();
         battleSessions = new ArrayList<>();
         battleSession.setBattleHistoryMessageId(1L);
         battleSession.setCreatedAt(LocalDateTime.now().minusDays(6));
         battleSessions.add(battleSession);
     }
-//    @Test
-//    void purgeBattleSessionTest() {
-//        when(battleSessionRepository.findAll()).thenReturn(battleSessions);
-//        doNothing().when(battleSessionRepository).deleteById(any());
-//        battleSessionPurge.purgeBattleSession();
-//        verify(battleSessionRepository, times(1)).deleteById(any());
-//    }
-//    @Test
-//    void purgeBattleSessionExceptionTest() {
-//        when(battleSessionRepository.findAll()).thenReturn(battleSessions);
-//        doThrow(new RuntimeException()).when(battleSessionRepository).deleteById(any());
-//        assertThrows(RuntimeException.class, () -> battleSessionPurge.purgeBattleSession());
-//    }
+    @Test
+    void purgeBattleSessionTest() {
+        when(battleSessionRepository.findAll()).thenReturn(battleSessions);
+        when(heroService.getHeroById(any())).thenReturn(tank);
+        doNothing().when(heroService).updateHero(any());
+        doNothing().when(battleSessionRepository).deleteById(any());
+        battleSessionPurge.purgeBattleSession();
+        verify(battleSessionRepository, times(1)).deleteById(any());
+    }
+    @Test
+    void purgeBattleSessionExceptionTest() {
+        when(heroService.getHeroById(any())).thenReturn(tank);
+        doNothing().when(heroService).updateHero(any());
+        when(battleSessionRepository.findAll()).thenReturn(battleSessions);
+        doThrow(new RuntimeException()).when(battleSessionRepository).deleteById(any());
+        assertThrows(RuntimeException.class, () -> battleSessionPurge.purgeBattleSession());
+    }
 }
