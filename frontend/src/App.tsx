a271c6e7fe0dddf2a936b4d21150590d2472efbd
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-07-07 16:03:55 -0400

    Fixed user register bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CustomNavBar from "./pages/CustomNavBar";
-import { FormEvent, useEffect, useState } from "react";
-import LoginForm from "./pages/LoginForm";
-import { getAuthToken, request, setAuthHeader } from "./helpers/axios_helper";
-import AccountSettings from "./pages/AccountSettings";
-import CreateNewHero from "./pages/CreateNewHero";
-import LeaderBoard from "./pages/LeaderBoard";
-import Inventory from "./pages/Inventory";
-import AboutUs from "./pages/AboutUs";
-import Home from "./pages/Home";
-import HowTo from "./pages/HowTo";
-import Cookies from "js-cookie";
+import React, { FormEvent, useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CustomNavBar from './pages/CustomNavBar';
+import LoginForm from './pages/LoginForm';
+import AccountSettings from './pages/AccountSettings';
+import CreateNewHero from './pages/CreateNewHero';
+import LeaderBoard from './pages/LeaderBoard';
+import Inventory from './pages/Inventory';
+import AboutUs from './pages/AboutUs';
+import Home from './pages/Home';
+import HowTo from './pages/HowTo';
+import { request, setAuthHeader } from './helpers/axios_helper';
+import Cookies from 'js-cookie';
 
 function App() {
   const [currentUser, setCurrentUser] = useState({
     userName: '',
     id: 0,
     loggedIn: false,
-}) 
+  });
 
   const logout = () => {
     setCurrentUser((prev) => ({
@@ -34,14 +34,14 @@ function App() {
       password: password,
     })
       .then((response) => {
-        Cookies.set("storedId", response.data.id)
-        Cookies.set("storedUserName", response.data.userName)
+        Cookies.set('storedId', response.data.id);
+        Cookies.set('storedUserName', response.data.userName);
         setAuthHeader(response.data.token);
-        setCurrentUser(() => ({
+        setCurrentUser({
           id: response.data.id,
           userName: response.data.userName,
           loggedIn: true,
-        }));
+        });
       })
       .catch((error) => {
         setAuthHeader(null);
@@ -49,76 +49,60 @@ function App() {
   };
 
   const onRegister = (username: string, password: string) => {
-    // event.preventDefault();
-      request('POST', `${import.meta.env.VITE_REACT_APP_URL}/register`, {
-
+    request('POST', `${import.meta.env.VITE_REACT_APP_URL}/register`, {
       login: username,
       password: password,
     })
       .then((response) => {
-        Cookies.set("storedId", response.data.id)
-        Cookies.set("storedUserName", response.data.userName)
+        Cookies.set('storedId', response.data.id);
+        Cookies.set('storedUserName', response.data.userName);
         setAuthHeader(response.data.token);
-        setCurrentUser(() => ({
+        setCurrentUser({
           id: response.data.id,
           userName: response.data.userName,
           loggedIn: true,
-        }));
+        });
       })
       .catch((error) => {
         setAuthHeader(null);
       });
   };
-  
-  useEffect(()=>{
-    const auth = Cookies.get("AuthHeader")
-    const storedId = Cookies.get("storedId");
-    const storedUserName = Cookies.get("storedUserName");
-    if (storedId && storedUserName && auth){
+
+  useEffect(() => {
+    const auth = Cookies.get('AuthHeader');
+    const storedId = Cookies.get('storedId');
+    const storedUserName = Cookies.get('storedUserName');
+    if (storedId && storedUserName && auth) {
       setAuthHeader(auth);
-      setCurrentUser(() => ({
+      setCurrentUser({
         id: Number(storedId),
         userName: storedUserName,
         loggedIn: true,
-      }));
+      });
     }
-  })
+  }, []);
 
   return (
     <BrowserRouter>
-      {/* <div className=""> */}
-        <CustomNavBar pageTitle="Battle Jawn" onLogout={logout} isLoggedIn={currentUser.loggedIn}/>
-        {/* <div className="background-jawn"> */}
-        <Routes>
-            {currentUser.loggedIn && (
-              <Route key="my-heroes" path="/" element={<Home props={currentUser} />} />
-            )}
-            {!currentUser.loggedIn && (
-              <Route key="login" path="/" element={<LoginForm onLogin={onLogin} onRegister={onRegister} />} />
-            )}
-            {currentUser.loggedIn && (
-              <Route key="create-hero" path="/create-hero" element={ <CreateNewHero props={currentUser} />} />
-            )}
-            {currentUser.loggedIn && (
-              <Route key="leader-board" path="/leader-board" element={ <LeaderBoard props={currentUser} />} />
-            )}
-            {currentUser.loggedIn && (
-              <Route key="account-settings" path="/account-settings" element={ <AccountSettings props={currentUser} logout={logout} />} />
-            )}
-            {currentUser.loggedIn && (
-              <Route key="inventory" path="/inventory" element={ <Inventory props={currentUser} />} />
-            )}
-            {currentUser.loggedIn && (
-              <Route key="about-us" path="/about-us" element={ <AboutUs />} />
-            )}
-            {currentUser.loggedIn && (
-              <Route key="how-to" path="/how-to" element={ <HowTo />} />
-            )}
-          </Routes>
-        {/* </div> */}
-      {/* </div> */}
+      <CustomNavBar pageTitle="Battle Jawn" onLogout={logout} isLoggedIn={currentUser.loggedIn} />
+      <Routes>
+        {currentUser.loggedIn && <Route key="my-heroes" path="/" element={<Home props={currentUser} />} />}
+        {!currentUser.loggedIn && <Route key="login" path="/" element={<LoginForm onLogin={onLogin} onRegister={onRegister} />} />}
+        {currentUser.loggedIn && <Route key="create-hero" path="/create-hero" element={<CreateNewHero props={currentUser} />} />}
+        {currentUser.loggedIn && <Route key="leader-board" path="/leader-board" element={<LeaderBoard props={currentUser} />} />}
+        {currentUser.loggedIn && (
+          <Route
+            key="account-settings"
+            path="/account-settings"
+            element={<AccountSettings props={currentUser} logout={logout} />}
+          />
+        )}
+        {currentUser.loggedIn && <Route key="inventory" path="/inventory" element={<Inventory props={currentUser} />} />}
+        {currentUser.loggedIn && <Route key="about-us" path="/about-us" element={<AboutUs />} />}
+        {currentUser.loggedIn && <Route key="how-to" path="/how-to" element={<HowTo />} />}
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- frontend/src/pages/LoginForm.tsx
+++ frontend/src/pages/LoginForm.tsx
@@ -3,7 +3,7 @@ import Container from '../components/Container';
 import Controls from '../components/Controls';
 import Display from '../components/Display';
 import PageName from '../components/PageName';
-import '../styling/LoginForm.css'
+import '../styling/LoginForm.css';
 import Cookies from 'js-cookie';
 
 interface LoginFormProps {
@@ -15,13 +15,13 @@ const LoginForm: React.FC<LoginFormProps> = ({ onLogin, onRegister }) => {
   const [activeButton, setActiveButton] = useState('Login');
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
-  const [confirmationPassword, setConfirmationPassword] = useState("");
-  const [message, setMessage] = useState("");
+  const [confirmationPassword, setConfirmationPassword] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleTabClick = (button: string) => {
-      setActiveButton(button);
-      Cookies.set("LoginFormTab", button)
-    };
+    setActiveButton(button);
+    Cookies.set('LoginFormTab', button);
+  };
 
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -35,151 +35,143 @@ const LoginForm: React.FC<LoginFormProps> = ({ onLogin, onRegister }) => {
     onLogin(e, login, password);
   };
 
-  const onSubmitRegister = () => {
-
-    if (password === confirmationPassword){
-      onRegister(login, password);  
+  const onSubmitRegister = (e: FormEvent) => {
+    e.preventDefault();
+    if (password === confirmationPassword) {
+      onRegister(login, password);
     } else {
-      setMessage("Passwords must match.");
+      setMessage('Passwords must match.');
     }
   };
 
-  useEffect(()=>{
-    const storedTab = Cookies.get("LoginFormTab");
+  useEffect(() => {
+    const storedTab = Cookies.get('LoginFormTab');
     if (storedTab) {
       setActiveButton(storedTab);
     }
-  })
+  }, []);
 
-    return (
-      <>
-        <Container>
-          <PageName>
-            <div className="page-name-column-1">
-              {/* <button className="page-name-btn">Sign Out</button> */}
+  return (
+    <Container>
+      <PageName>
+        <div className="page-name-column-1"></div>
+        <div className="page-name-column-2">
+          <div className="page-name-txt">Welcome</div>
+        </div>
+        <div className="page-name-column-3"></div>
+      </PageName>
+      <Display>
+        <div className="parent-jawn">
+          <div className="child-jawn">
+            <div className="tab-jawn">
+              {activeButton === 'Login' && (
+                <form className="form-jawn" onSubmit={onSubmitLogin}>
+                  <div className="username-jawn">
+                    <input
+                      type="login"
+                      value={login}
+                      name="login"
+                      className="form-control"
+                      placeholder="Username"
+                      onChange={onChangeHandler}
+                    />
+                  </div>
+                  <div className="password-jawn">
+                    <input
+                      type="password"
+                      value={password}
+                      name="password"
+                      className="form-control"
+                      placeholder="Password"
+                      onChange={onChangeHandler}
+                    />
+                  </div>
+                  <div className="login-submit-btn-jawn">
+                    <button type="submit" className="login-submit-btn">
+                      Sign In
+                    </button>
+                  </div>
+                </form>
+              )}
+
+              {activeButton === 'Register' && (
+                <form onSubmit={onSubmitRegister}>
+                  <div className="username-jawn">
+                    <input
+                      type="text"
+                      value={login}
+                      name="login"
+                      className="form-control"
+                      placeholder="Username"
+                      onChange={onChangeHandler}
+                    />
+                  </div>
+                  <div className="password-jawn">
+                    <input
+                      type="password"
+                      value={password}
+                      name="password"
+                      className="form-control"
+                      placeholder="Password"
+                      onChange={onChangeHandler}
+                    />
+                    <input
+                      type="password"
+                      value={confirmationPassword}
+                      name="confirmation-password"
+                      className="form-control"
+                      placeholder="Confirm Password"
+                      onChange={onChangeHandler}
+                    />
+                    <p>{message}</p>
+                  </div>
+                  <div className="login-submit-btn-jawn">
+                    <button type="submit" className="login-submit-btn">
+                      Register
+                    </button>
+                  </div>
+                </form>
+              )}
             </div>
-            <div className="page-name-column-2">
-              <div className="page-name-txt">
-              Welcome
-              </div>
-            </div>
-            <div className="page-name-column-3">
-              {/* <button className="page-name-btn">New Hero</button> */}
-            </div>
-          </PageName>
-          <Display>
-            <div className="parent-jawn">
-              <div className="child-jawn">
-
-              <div className="tab-jawn">
-                {activeButton === 'Login' &&
-                  <form 
-                    className="form-jawn"
-                    onSubmit={onSubmitLogin}
-                  >
-                    <div className="username-jawn">
-                      <input
-                        type="login"
-                        value={login}
-                        name="login"
-                        className="form-control"
-                        placeholder='Username'
-                        onChange={onChangeHandler}
-                      /> 
-                    </div>
-
-                    <div className="password-jawn">
-                      <input
-                        type="password"
-                        value={password}
-                        name="password"
-                        className="form-control"
-                        placeholder='Password'
-                        onChange={onChangeHandler}
-                      />
-                    </div>
-
-                    
-                    <div className="login-submit-btn-jawn">
-                      <button type="submit" className="login-submit-btn">
-                        Sign In
-                      </button>
-                    </div>
-                  </form>
-                }
-              
-
-                {activeButton === 'Register' &&
-                  <form 
-                    onSubmit={onSubmitRegister}
-                  >
-                    <div className="username-jawn">
-                      
-                      <input
-                        type="text"
-                        value={login}
-                        name="login"
-                        className="form-control"
-                        placeholder='Username'
-                        onChange={onChangeHandler}
-                      />
-                    </div>
-
-                    <div className="password-jawn">
-                      
-                      <input
-                        type="password"
-                        value={password}
-                        name="password"
-                        className="form-control"
-                        placeholder='Password'
-                        onChange={onChangeHandler}
-                      />
-                      <input
-                        type="password"
-                        value={confirmationPassword}
-                        name="confirmation-password"
-                        className="form-control"
-                        placeholder='Confirm Password'
-                        onChange={onChangeHandler}
-                      />
-                      <p>{message}</p>
-
-                    </div>
-                    <div className="login-submit-btn-jawn">
-                      <button onClick={() => onSubmitRegister()} type="submit" className="login-submit-btn">
-                        Register
-                      </button>
-                    </div>
-                  </form>
-                }
-              </div>
-              </div>
-              <div className="display-jawn-tabs">
-                <button className={activeButton === 'Login' ? 'active' : ''} onClick={()=> handleTabClick("Login")}>Login</button>
-                <button className={activeButton === 'Register' ? 'active' : ''} onClick={()=> handleTabClick("Register")}>Register</button>
-              </div>
-            </div>
-          </Display>
-          <Controls>
-            <>
-              <div className="controls-left">
-                <button className="controls-btn"></button>
-                <button className="controls-btn"></button>
-                <button className="controls-btn"></button>                    
-              </div>
-              <div className="controls-right">
-                <button className="controls-btn"></button>
-                <button className="controls-btn" onClick={()=> handleTabClick("Login")}>Left</button>
-                <button className="controls-btn"></button>
-                <button className="controls-btn" onClick={()=> handleTabClick("Register")}>Right</button>
-                <button className="controls-btn"></button>
-              </div>
-            </>
-          </Controls>
-      </Container>
-    </>
+          </div>
+          <div className="display-jawn-tabs">
+            <button
+              className={activeButton === 'Login' ? 'active' : ''}
+              onClick={() => handleTabClick('Login')}
+            >
+              Login
+            </button>
+            <button
+              className={activeButton === 'Register' ? 'active' : ''}
+              onClick={() => handleTabClick('Register')}
+            >
+              Register
+            </button>
+          </div>
+        </div>
+      </Display>
+      <Controls>
+        <>
+          <div className="controls-left">
+            <button className="controls-btn"></button>
+            <button className="controls-btn"></button>
+            <button className="controls-btn"></button>
+          </div>
+          <div className="controls-right">
+            <button className="controls-btn"></button>
+            <button className="controls-btn" onClick={() => handleTabClick('Login')}>
+              Left
+            </button>
+            <button className="controls-btn"></button>
+            <button className="controls-btn" onClick={() => handleTabClick('Register')}>
+              Right
+            </button>
+            <button className="controls-btn"></button>
+          </div>
+        </>
+      </Controls>
+    </Container>
   );
-}
+};
 
 export default LoginForm;
