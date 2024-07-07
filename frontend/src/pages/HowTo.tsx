3756828091d5711f855e88bd036161d61fdeb6a5
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-07-07 18:34:13 -0400

    Updated How To Component

--- frontend/src/pages/HowTo.tsx
+++ frontend/src/pages/HowTo.tsx
@@ -76,6 +76,46 @@ const HowTo = () => {
                             <button className={activeButton === 'Play' ? 'active' : ''} onClick={()=> handleClickSpecificTab("Play")}>Play</button>
                             <button className={activeButton === 'Leader Board' ? 'active' : ''} onClick={()=> handleClickSpecificTab("Leader Board")}>Leader Board</button>
                         </div>
+                            {activeButton === 'Battle' &&
+                                <div className="battle">
+                                    <p style={{color: "white"}}>Strike: Generates 1 Power.</p>
+                                    <p style={{color: "white"}}>Impale: Consumes 2 Power.</p>
+                                    <p style={{color: "white"}}>Block: Successful blocks generate Max Power.</p>
+                                    <p style={{color: "white"}}>Wand: Generates 1 Spirit/Magic.</p>
+                                    <p style={{color: "white"}}>Holy: Consumes 2 Spirit.</p>
+                                    <p style={{color: "white"}}>Heal: Consumes 1 Spirit.</p>
+                                    <p style={{color: "white"}}>Stab: Generates 1 Energy.</p>
+                                    <p style={{color: "white"}}>BackStab: Consumes 3 Energy.</p>
+                                    <p style={{color: "white"}}>Steal: Consumes 1 Energy.</p>
+                                    <p style={{color: "white"}}>FireBlast: Consumes 3 Magic.</p>
+                                    <p style={{color: "white"}}>IceBlast: Consumes 1 Magic.</p>
+                                </div>
+                            }
+
+                            {activeButton === "Play" &&
+                                <div className="battle">
+                                    <p style={{color: "white"}}>Arena: Enter to fight enemies.</p>
+                                    <p style={{color: "white"}}>Shop: Enter to purchase and sell items.</p>
+                                    <p style={{color: "white"}}>Leaving Town: Currently Unavailable.</p>
+                                    <p style={{color: "white"}}>Entering Town Homes: Currently Unavailable.</p>
+                                    <p style={{color: "white"}}>Hospital: Currently Unavailable.</p>
+                                    <p style={{color: "white"}}>Hostel: Currently Unavailable.</p>
+                                    <p style={{color: "white"}}>NPCs: Currently Unavailable.</p>
+                                    <p style={{color: "white"}}>Quests: Currently Unavailable.</p>
+                                </div>
+                            }
+
+                            {activeButton === "Leader Board" &&
+                                <div className="battle">
+                                    <p style={{color: "white"}}>Press up or down to scroll through the current Top 5 heroes</p>
+                                </div>
+                            }
+
+                            {activeButton === "Heroes" &&
+                                <div className="battle">
+                                    <p style={{color: "white"}}>Talent Tree: Currently Unavailable.</p>
+                                </div>
+                            }
                     </div>
             </Display>
             <Controls>
