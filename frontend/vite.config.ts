14137abd65c438febed5f69068775e2f1822a626
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-07-09 12:29:45 -0400

    Update frontend config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: Number(process.env.PORT) || 3000,
+//   },
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+// Load environment variables based on NODE_ENV
+const loadEnv = (mode: string) => {
+  const env = dotenv.config({
+    path: `.env.${mode}`,
+  }).parsed;
+
+  return Object.keys(env || {}).reduce((prev, key) => {
+    prev[`process.env.${key}`] = JSON.stringify(env[key]);
+    return prev;
+  }, {});
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT) || 3000,
-  },
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: Number(process.env.PORT) || 3000,
+    },
+    define: {
+      ...loadEnv(mode),
+    },
+  };
+});
