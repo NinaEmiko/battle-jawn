bc176637b72f8bc92324321268e2fcbf212c574d
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-08-01 08:34:15 -0400

    Remove outdated tests

--- backend/src/test/java/com/battlejawn/Controllers/EnemyMoveControllerTest.java
+++ backend/src/test/java/com/battlejawn/Controllers/EnemyMoveControllerTest.java
@@ -1,49 +1,49 @@
-package com.battlejawn.Controllers;
-
-import com.battlejawn.Config.JsonParser;
-import com.battlejawn.DTO.EnemyMoveDTO;
-import com.battlejawn.DTO.HeroMoveDTO;
-import com.battlejawn.Service.EnemyMoveService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
-
-@ExtendWith(MockitoExtension.class)
-class EnemyMoveControllerTest {
-    @Mock
-    EnemyMoveService enemyMoveService;
-    @Mock
-    JsonParser jsonParser;
-    @Mock
-    EnemyMoveDTO enemyMoveDTO;
-    @InjectMocks
-    EnemyMoveController enemyMoveController;
-    @BeforeEach
-    void setup(){
-        enemyMoveDTO = new EnemyMoveDTO();
-
-    }
-    @Test
-    void enemyMoveTest(){
-        when(jsonParser.extractBattleSessionId(anyString())).thenReturn(1L);
-        when(enemyMoveService.enemyMove(anyLong())).thenReturn(enemyMoveDTO);
-        enemyMoveController.enemyMove(anyString());
-        verify(enemyMoveService, times(1)).enemyMove(anyLong());
-    }
-    @Test
-    void enemyMoveNullTest(){
-        when(jsonParser.extractBattleSessionId(anyString())).thenReturn(1L);
-        when(enemyMoveService.enemyMove(anyLong())).thenReturn(null);
-        enemyMoveController.enemyMove(anyString());
-        verify(enemyMoveService, times(1)).enemyMove(anyLong());
-    }
-
-}
+//package com.battlejawn.Controllers;
+//
+//import com.battlejawn.Config.JsonParser;
+//import com.battlejawn.DTO.EnemyMoveDTO;
+//import com.battlejawn.DTO.HeroMoveDTO;
+//import com.battlejawn.Service.EnemyMoveService;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.ArgumentMatchers.anyLong;
+//import static org.mockito.ArgumentMatchers.anyString;
+//import static org.mockito.Mockito.*;
+//
+//@ExtendWith(MockitoExtension.class)
+//class EnemyMoveControllerTest {
+//    @Mock
+//    EnemyMoveService enemyMoveService;
+//    @Mock
+//    JsonParser jsonParser;
+//    @Mock
+//    EnemyMoveDTO enemyMoveDTO;
+//    @InjectMocks
+//    EnemyMoveController enemyMoveController;
+//    @BeforeEach
+//    void setup(){
+//        enemyMoveDTO = new EnemyMoveDTO();
+//
+//    }
+//    @Test
+//    void enemyMoveTest(){
+//        when(jsonParser.extractBattleSessionId(anyString())).thenReturn(1L);
+//        when(enemyMoveService.enemyMove(anyLong())).thenReturn(enemyMoveDTO);
+//        enemyMoveController.enemyMove(anyString());
+//        verify(enemyMoveService, times(1)).enemyMove(anyLong());
+//    }
+//    @Test
+//    void enemyMoveNullTest(){
+//        when(jsonParser.extractBattleSessionId(anyString())).thenReturn(1L);
+//        when(enemyMoveService.enemyMove(anyLong())).thenReturn(null);
+//        enemyMoveController.enemyMove(anyString());
+//        verify(enemyMoveService, times(1)).enemyMove(anyLong());
+//    }
+//
+//}
