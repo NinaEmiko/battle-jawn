5398be22ebe192d70b7cc4797ceda9f7e6ec0884
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-08-03 01:55:58 -0400

    Updated coming soon component

--- frontend/src/components/HowToComponents/HowToComingSoon.tsx
+++ frontend/src/components/HowToComponents/HowToComingSoon.tsx
@@ -5,7 +5,8 @@ const HowToComingSoon = () => {
             <p className="text-jawn">Leaving Town</p>
             <p className="text-jawn">Hospital</p>
             <p className="text-jawn">NPCs</p>
-            <p className="text-jawn">Talent Tree</p>      
+            <p className="text-jawn">Stealth Talent Tree</p>  
+            <p className="text-jawn">Energized and Preparation Talents</p>          
             <p className="text-jawn">Quests</p>   
         </div>
     )
